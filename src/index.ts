e510a22fc1ba9fe8f24c2deca3068e60aff5d34a
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-15 11:24:37 -0400

    finished implementation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-function prepend(numSpaces: number, inputString: string) {
-  // your code here
+
+// Write a TypeScript function that adds a number of spaces to the front of a
+// `string`.
+
+
+function prepend(numSpaces: number, inputString: string): string {
+  return inputString.padStart(inputString.length + numSpaces, " ");
 }
+
+let spaces: number = 10;
+let input: string = "input";
+console.log(prepend(spaces, input));
